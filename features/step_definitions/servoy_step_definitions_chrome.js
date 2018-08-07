4eff92cfefd8857144e3db2de720acdf6e189eda
azijlstra <azijlstra@servoy.com>
2018-08-07 17:38:37 +0200

    Ambigous test step fix

--- features/step_definitions/servoy_step_definitions_chrome.js
+++ features/step_definitions/servoy_step_definitions_chrome.js
@@ -43,7 +43,7 @@ defineSupportCode(({ Given, Then, When, Before, After }) => {
 		});
 	});
 
-	Given('I go to the test domain', { timeout: 60 * 1000}, function(callback) {
+	Given('I navigate to the test domain', { timeout: 60 * 1000}, function(callback) {
 		console.log("Opening testdomain URL: " + browser.params.testDomainURL);
 		browser.get(browser.params.testDomainURL).then(function () {
 			wrapUp(callback, "navigateURLEvent")
