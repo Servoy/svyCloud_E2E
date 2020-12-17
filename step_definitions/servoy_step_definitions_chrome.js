354e346cb20c1d8d3af1f5a584fb09c2618d02da
Alex Zijlstra <azijlstra@servoy.com>
2020-12-17 14:55:53 +0100

    Fix bootstrap select issue

--- step_definitions/servoy_step_definitions_chrome.js
+++ step_definitions/servoy_step_definitions_chrome.js
@@ -3452,8 +3452,8 @@ defineSupportCode(({ Given, Then, When, Before, After }) => {
 		browser.wait(EC.presenceOf(fComponent), 30 * 1000, 'Formcomponent not found!').then(function () {
 			var checkbox = fComponent.element(by.css(`data-bootstrapcomponents-checkbox[data-svy-name='${elementName}']`));
 			browser.wait(EC.presenceOf(checkbox), 15 * 1000, 'Checkbox not found!').then(function () {
-				checkbox.isSelected().then(function (isChecked) {
-					if ((isChecked && checkboxOption.toLowerCase() === "checked") || (!isChecked && checkboxOption.toLowerCase() === "unchecked")) {
+				checkbox.element(by.css('input')).isSelected().then(function (isChecked) {
+					if ((isChecked && checkboxOption.toLowerCase() === "unchecked") || (!isChecked && checkboxOption.toLowerCase() === "checked")) {
 						checkbox.element(by.css('span')).click().then(function () {
 							wrapUp(callback, "checkboxEvent");
 						})
